d53231cdd7bc972ce34e0d0cc78a4bd5721009d1
kthaovd13 <58517232+kthaovd13@users.noreply.github.com>
2020-04-19 20:01:55 -0500

    completed MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import Photo from "./components/Photo"
 
 function App() {
   return (
     <div className="App">
       <h1>NASA</h1>
       <p>Photo of the Day</p>
+        <div>
+          <Photo />
+        </div>
     </div>
   );
 }
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from "react"
+import React, { useState, useEffect } from "react"
 import axios from "axios"
 import Photoblock from "./Photoblock"
 
 
 
-const Photo = (() => {
+const Photo = () => {
 
     const [planet, setPlanet] = useState([])
 
     useEffect (() => {
-        axios.get(`https://api.nasa.gov/planetary/apod?api_key=BOplO9bKJcc3aIm1ICNCwTXVI9Hi8QugwJwO1Gl8`)
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=BOplO9bKJcc3aIm1ICNCwTXVI9Hi8QugwJwO1Gl8&date=2012-03-14`)
         .then(res => setPlanet(res.data))
         .catch(err => {console.log("The data was not returned", err)})
     }, [])
@@ -19,6 +19,6 @@ const Photo = (() => {
             <Photoblock planet={planet} />
         </div>
     )
-})
+}
 
 export default Photo
